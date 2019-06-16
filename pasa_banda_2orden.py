42b8032ded3ab11d359e8913176360bcd29d491b
Carlos <31170466+CarlosAngelChen@users.noreply.github.com>
2019-06-16 02:24:42 -0300

    Add files via upload

---
+++ pasa_banda_2orden.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+
+def pbanda():
+    k = float(input())
+    w0 = float(input())
+    etha = float(input())
+
+    ceros = [1, 0]
+    polos = [1/(w0**2), 2*etha/w0, 1]
+
+    sys = signal.TransferFunction(ceros, polos)
+
+    w, dB, phase = signal.bode(sys, w = None, n = 500)
+
+    f = w/np.pi
+
+    return w, f, k*dB, phase
+
+def pbanda_plot(w, f, dB, phase):
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+
+    ax1.semilogx(f, dB)
+    ax1.set_xlabel('Hz')
+    ax1.set_ylabel('dB')
+    ax1.set_title('Base 10')
+    ax1.grid(True)
+
+    ax2.semilogx(w, dB)
+    ax2.set_xlabel('rad/s')
+    ax2.set_ylabel('dB')
+    ax2.set_title('Base 2')
+    ax2.grid(True)
+
+    ax3.semilogx(f, dB, basex=2)
+    ax3.set_xlabel('Hz')
+    ax3.set_ylabel('dB')
+    ax3.set_title('Base 2')
+    ax3.grid(True)
+
+    ax4.semilogx(w, dB, basex=2)
+    ax4.set_xlabel('rad/s')
+    ax4.set_ylabel('dB')
+    ax4.set_title('Base 2')
+    ax4.grid(True)
+
+    fig.tight_layout()
+    plt.show()
+
+    return 0
+
+w, f, dB, phase = pbanda()
+pbanda_plot(w, f, dB, phase)
